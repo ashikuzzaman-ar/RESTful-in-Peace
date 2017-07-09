9061e5db6a92754b8c0d0654410816f316bd83fc
ashik <ashik@studevs>
2017-07-09 22:09:52 +0600

    Create : Added Doctor and Patient model under com.studevs.dummy.restful.in.peace.models.user package.

---
+++ src/main/java/com/studevs/dummy/restful/in/peace/models/user/Patient.java
@@ -0,0 +1,25 @@
+package com.studevs.dummy.restful.in.peace.models.user;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ashik
+ */
+@Entity
+@Table(name = "patients")
+public class Patient implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+}
