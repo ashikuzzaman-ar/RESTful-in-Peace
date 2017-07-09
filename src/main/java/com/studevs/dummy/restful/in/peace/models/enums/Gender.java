0019885a5f6a02ecfe6b0e414c3ece9d75134312
ashik <ashik@studevs>
2017-07-09 23:12:05 +0600

    Create : Added Gender enum under com.studevs.dummy.restful.in.peace.models.enums package.

---
+++ src/main/java/com/studevs/dummy/restful/in/peace/models/enums/Gender.java
@@ -0,0 +1,16 @@
+package com.studevs.dummy.restful.in.peace.models.enums;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ashik
+ */
+public enum Gender implements Serializable {
+
+    MALE, //This will be used for a male user.
+    FEMALE, //This will be used for a female user.
+    TRANSGENDER, //This will be used for a transgender user.
+    SHEMALE, //This will be used for a shemale user.
+    OTHER //his will be used for some special case which are not listed above.
+}
