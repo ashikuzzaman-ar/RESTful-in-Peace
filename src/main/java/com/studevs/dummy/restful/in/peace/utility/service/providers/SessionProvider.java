660b5546c33969411c269b250a60892ba73d9973
ashik <ashik@studevs>
2017-07-10 02:20:01 +0600

    Create : Added SessionProvider service class for providing Session objects.

---
+++ src/main/java/com/studevs/dummy/restful/in/peace/utility/service/providers/SessionProvider.java
@@ -0,0 +1,44 @@
+package com.studevs.dummy.restful.in.peace.utility.service.providers;
+
+import java.io.Serializable;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author ashik
+ */
+public class SessionProvider implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final SessionFactory SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
+
+    private Session session;
+
+    /**
+     * This method will provide Session object per call.
+     *
+     * @return
+     */
+    public Session getSession() {
+        return session;
+    }
+
+    /**
+     * This init method will be initialized in SessionProvider bean creation.
+     */
+    public void initSession() {
+
+        this.session = SESSION_FACTORY.openSession();
+    }
+
+    /**
+     * This method will destroy session object while destroying SessionProvider bean
+     */
+    public void destroySession() {
+
+        this.session.close();
+    }
+}
