b29fe3466d3b9de27c65852a13f455f5162f3db1
ashik <ashik@studevs>
2017-07-10 04:49:00 +0600

    Create : Added Role enum under com.studevs.dummy.restful.in.peace.models.enums package.

---
+++ src/main/java/com/studevs/dummy/restful/in/peace/models/enums/Role.java
@@ -0,0 +1,14 @@
+package com.studevs.dummy.restful.in.peace.models.enums;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ashik
+ */
+public enum Role implements Serializable {
+
+    PATIENT,
+    DOCTOR,
+    ADMIN
+}
