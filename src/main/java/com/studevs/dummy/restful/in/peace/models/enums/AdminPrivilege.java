590b9887248fd99a72ec828b00cdcd97864f569b
ashik <ashik@studevs>
2017-07-10 12:36:28 +0600

    Create : Added AdminPrivilege enum under com.studevs.dummy.restful.in.peace.models.enums package.

---
+++ src/main/java/com/studevs/dummy/restful/in/peace/models/enums/AdminPrivilege.java
@@ -0,0 +1,16 @@
+package com.studevs.dummy.restful.in.peace.models.enums;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ashik
+ */
+public enum AdminPrivilege implements Serializable {
+
+    ALL,
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE
+}
