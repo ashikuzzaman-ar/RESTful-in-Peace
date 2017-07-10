aaa669d663a0990c86fc9dc55364d3b9e219e4b1
ashik <ashik@studevs>
2017-07-10 19:40:08 +0600

    Update : Login service (token generation) has been implemented in AdminLoginController.

--- src/main/java/com/studevs/dummy/restful/in/peace/controllers/rest_controllers/AdminLoginController.java
+++ src/main/java/com/studevs/dummy/restful/in/peace/controllers/rest_controllers/AdminLoginController.java
@@ -1,8 +1,19 @@
 package com.studevs.dummy.restful.in.peace.controllers.rest_controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studevs.dummy.restful.in.peace.models.user.Admin;
+import com.studevs.dummy.restful.in.peace.utility.service.Encrypt;
 import com.studevs.dummy.restful.in.peace.utility.service.providers.BeanProvider;
+import com.studevs.dummy.restful.in.peace.utility.service.providers.SessionProvider;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +27,86 @@ public class AdminLoginController extends BeanProvider {
 
     private static final long serialVersionUID = 1L;
     private Admin admin;
+    private SessionProvider sessionProvider;
+    private ObjectMapper mapper;
+    private List<String> message;
+    private Map<String, List<String>> messages;
     private String json;
 
-    @RequestMapping(value = "login")
-    protected String test(HttpServletRequest request) {
+    private void initializer(HttpServletRequest request) {
 
         this.createContext(request);
+        this.mapper = this.getBean("mapper");
+        this.message = this.getBean("message");
+        this.message.clear();
+        this.messages = this.getBean("messages");
+        this.messages.put("messages", this.message);
+        this.sessionProvider = this.getBean("session");
         this.json = "";
+    }
+
+    @RequestMapping(value = "login")
+    @SuppressWarnings("unchecked")
+    protected String test(HttpServletRequest request,
+            @ModelAttribute Admin adminModel,
+            BindingResult bindingResult) {
+
+        this.initializer(request);
+
         try {
 
-        } catch (Exception e) {
+            if (!bindingResult.hasErrors()) {
+
+                if ((adminModel.getUsername() != null && !adminModel.getUsername().isEmpty()) && (adminModel.getPassword() != null && !adminModel.getPassword().isEmpty())) {
+
+                    Session session = this.sessionProvider.getSession();
+                    Transaction transaction = null;
+                    Query<Admin> query;
+
+                    try {
+
+                        transaction = session.beginTransaction();
+                        query = session.createQuery("FROM Admin A WHERE A.username = :id_1");
+                        query.setParameter("id_1", adminModel.getUsername());
+                        Admin adminFromDB = query.uniqueResult();
+                        transaction.commit();
+
+                        Encrypt encrypt = this.getBean("encrypt");
+                        if (adminFromDB == null) {
+
+                            this.message.add("User doesn't exist!");
+                            this.json = this.mapper.writeValueAsString(this.messages);
+                        } else if (encrypt.generateHash(adminModel.getPassword(), adminFromDB.getId()).equals(adminFromDB.getPassword())) {
+
+                            adminFromDB.setToken(encrypt.generateHash((adminFromDB.getUsername() + this.getBean("date") + adminFromDB.getPassword() + adminFromDB.getAdminPrivilege()), adminFromDB.getId()));
+                            this.admin = this.getBean("admin");
+                            this.admin.replicate(adminFromDB);
+                            this.json = this.mapper.writeValueAsString(this.admin);
+                        } else {
+
+                            this.message.add("Password is incorrect!");
+                            this.json = this.mapper.writeValueAsString(this.messages);
+                        }
+                    } catch (JsonProcessingException e) {
+
+                        if (transaction != null) {
+
+                            transaction.rollback();
+                        }
+
+                        this.logger(e, null, null);
+                    }
+                } else {
+
+                    this.message.add("Username and/or password is empty!");
+                    this.json = this.mapper.writeValueAsString(this.messages);
+                }
+            } else {
+
+                this.message.add("Data binding error!");
+                this.json = this.mapper.writeValueAsString(this.messages);
+            }
+        } catch (JsonProcessingException e) {
 
             this.logger(e, null, null);
         }
