867af57b243fbac78d0df5e9fd1f9d3a02c792f8
ashik <ashik@studevs>
2017-07-11 03:00:29 +0600

    Create : Added DoctorLoginController and PatientLoginController under com.studevs.dummy.restful.in.peace.controllers.rest_controllers package.

---
+++ src/main/java/com/studevs/dummy/restful/in/peace/controllers/rest_controllers/DoctorLoginController.java
@@ -0,0 +1,17 @@
+package com.studevs.dummy.restful.in.peace.controllers.rest_controllers;
+
+import com.studevs.dummy.restful.in.peace.utility.service.providers.BeanProvider;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ashik
+ */
+@RestController
+@RequestMapping(value = "")
+public class DoctorLoginController extends BeanProvider {
+
+    private static final long serialVersionUID = 1L;
+
+}
